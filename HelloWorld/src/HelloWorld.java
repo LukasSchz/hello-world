2097269f2f2d940ede1363052b57fa95c0f75c54
LukasSchz <46381400+LukasSchz@users.noreply.github.com>
2019-01-04 19:11:37 +0100

    Changed first project
    New printline

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("First change!");
+		System.out.println("");
+		System.out.println("Second change!");
 	}
 }
